8766941c130e20311ac6b968e115abfb8a1a90d1
dmx20070206 <dmx20070206@163.com>
2026-01-05 19:03:10 +0800

    ✨ feat: Submit check0

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -3,40 +3,58 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream(uint64_t capacity) : capacity_(capacity), buffer_(std::vector<char>()) {}
 
 // Push data to stream, but only as much as available capacity allows.
-void Writer::push( string data )
+void Writer::push(string data)
 {
-  // Your code here (and in each method below)
-  debug( "Writer::push({}) not yet implemented", data );
+    // If stream is closed, set error
+    if (is_closed_ && !data.empty())
+    {
+        set_error();
+        return;
+    }
+
+    // Trim data to available capacity
+    uint64_t cur_capacity = available_capacity();
+    if (data.size() > cur_capacity)
+        data = data.substr(0, cur_capacity);
+
+    // Append data to buffer
+    for (char c : data)
+        buffer_.push_back(c);
+    bytes_pushed_ += data.size();
+
+    debug("Writer::push({})", data);
 }
 
 // Signal that the stream has reached its ending. Nothing more will be written.
 void Writer::close()
 {
-  debug( "Writer::close() not yet implemented" );
+    is_closed_ = true;
+    debug("Writer::close()");
 }
 
 // Has the stream been closed?
 bool Writer::is_closed() const
 {
-  debug( "Writer::is_closed() not yet implemented" );
-  return {}; // Your code here.
+    debug("Writer::is_closed() -> {}", is_closed_);
+    return is_closed_;
 }
 
 // How many bytes can be pushed to the stream right now?
 uint64_t Writer::available_capacity() const
 {
-  debug( "Writer::available_capacity() not yet implemented" );
-  return {}; // Your code here.
+    uint64_t available_capacity = capacity_ - (bytes_pushed_ - bytes_popped_);
+    debug("Writer::available_capacity() -> {}", available_capacity);
+    return available_capacity;
 }
 
 // Total number of bytes cumulatively pushed to the stream
 uint64_t Writer::bytes_pushed() const
 {
-  debug( "Writer::bytes_pushed() not yet implemented" );
-  return {}; // Your code here.
+    debug("Writer::bytes_pushed() -> {}", bytes_pushed_);
+    return bytes_pushed_;
 }
 
 // Peek at the next bytes in the buffer -- ideally as many as possible.
@@ -45,33 +63,51 @@ uint64_t Writer::bytes_pushed() const
 // the caller to do a lot of extra work.
 string_view Reader::peek() const
 {
-  debug( "Reader::peek() not yet implemented" );
-  return {}; // Your code here.
+    debug("Reader::peek() called");
+    if (buffer_.empty())
+    {
+        return {};
+    }
+
+    return string_view(buffer_.data(), buffer_.size());
 }
 
 // Remove `len` bytes from the buffer.
-void Reader::pop( uint64_t len )
+void Reader::pop(uint64_t len)
 {
-  debug( "Reader::pop({}) not yet implemented", len );
+    if (is_finished() || len > bytes_buffered())
+    {
+        set_error();
+        return;
+    }
+
+    bytes_popped_ += len;
+    buffer_.erase(buffer_.begin(), buffer_.begin() + len);
+
+    debug("Reader::pop({})", len);
 }
 
 // Is the stream finished (closed and fully popped)?
 bool Reader::is_finished() const
 {
-  debug( "Reader::is_finished() not yet implemented" );
-  return {}; // Your code here.
+    bool is_empty = bytes_buffered() == 0;
+    bool is_finished = is_closed_;
+
+    debug("Reader::is_finished() -> {}", is_empty && is_finished);
+
+    return is_empty && is_finished;
 }
 
 // Number of bytes currently buffered (pushed and not popped)
 uint64_t Reader::bytes_buffered() const
 {
-  debug( "Reader::bytes_buffered() not yet implemented" );
-  return {}; // Your code here.
+    debug("Reader::bytes_buffered() -> {}", bytes_pushed_ - bytes_popped_);
+    return bytes_pushed_ - bytes_popped_;
 }
 
 // Total number of bytes cumulatively popped from stream
 uint64_t Reader::bytes_popped() const
 {
-  debug( "Reader::bytes_popped() not yet implemented" );
-  return {}; // Your code here.
+    debug("Reader::bytes_popped() -> {}", bytes_popped_);
+    return bytes_popped_;
 }
--- util/address.hh
+++ util/address.hh
@@ -16,64 +16,59 @@ public:
   class Raw
   {
   public:
-    sockaddr_storage storage {}; //!< The wrapped struct itself.
+    sockaddr_storage storage{}; //!< The wrapped struct itself.
     // NOLINTBEGIN (*-explicit-*)
-    operator sockaddr*();
-    operator const sockaddr*() const;
+    operator sockaddr *();
+    operator const sockaddr *() const;
     // NOLINTEND (*-explicit-*)
   };
 
 private:
-  socklen_t size_; //!< Size of the wrapped address.
-  Raw address_ {}; //!< A wrapped [sockaddr_storage](@ref man7::socket) containing the address.
+  socklen_t size_; //< Size of the wrapped address.
+  Raw address_{};  //< A wrapped [sockaddr_storage](@ref man7::socket) containing the address.
 
-  //! Constructor from ip/host, service/port, and hints to the resolver.
-  Address( const std::string& node, const std::string& service, const addrinfo& hints );
+  // (Constructer) Constructor from ip/host, service/port, and hints to the resolver.
+  Address(const std::string &node, const std::string &service, const addrinfo &hints);
 
 public:
-  //! Construct by resolving a hostname and servicename.
-  Address( const std::string& hostname, const std::string& service );
+  // (Constructer) Construct by resolving a hostname and servicename.
+  Address(const std::string &hostname, const std::string &service);
+  // (Constructer) Construct from dotted-quad string ("18.243.0.1") and numeric port.
+  explicit Address(const std::string &ip, std::uint16_t port = 0);
+  // (Constructer) Construct from a [sockaddr *](@ref man7::socket).
+  Address(const sockaddr *addr, std::size_t size);
+  // (Constructer) Default constructor
+  Address() : Address(nullptr, 0) {}
 
-  //! Construct from dotted-quad string ("18.243.0.1") and numeric port.
-  explicit Address( const std::string& ip, std::uint16_t port = 0 );
-
-  //! Construct from a [sockaddr *](@ref man7::socket).
-  Address( const sockaddr* addr, std::size_t size );
-
-  //! Default constructor
-  Address() : Address( nullptr, 0 ) {}
-
-  //! Equality comparison.
-  bool operator==( const Address& other ) const;
-  bool operator!=( const Address& other ) const { return not operator==( other ); }
+  // Equality comparison.
+  bool operator==(const Address &other) const;
+  bool operator!=(const Address &other) const { return not operator==(other); }
 
   //! \name Conversions
   //!@{
-
-  //! Dotted-quad IP address string ("18.243.0.1") and numeric port.
+  // (Getter) Dotted-quad IP address string ("18.243.0.1") and numeric port.
   std::pair<std::string, uint16_t> ip_port() const;
-  //! Dotted-quad IP address string ("18.243.0.1").
+  // (Getter) Dotted-quad IP address string ("18.243.0.1").
   std::string ip() const { return ip_port().first; }
-  //! Numeric port (host byte order).
+  // (Getter) Numeric port (host byte order).
   uint16_t port() const { return ip_port().second; }
-  //! Numeric IP address as an integer (i.e., in [host byte order](\ref man3::byteorder)).
+  // (Getter) Numeric IP address as an integer (i.e., in [host byte order](\ref man3::byteorder)).
   uint32_t ipv4_numeric() const;
-  //! Create an Address from a 32-bit raw numeric IP address
-  static Address from_ipv4_numeric( uint32_t ip_address );
-  //! Human-readable string, e.g., "8.8.8.8:53".
+  // (Getter) Human-readable string, e.g., "8.8.8.8:53".
   std::string to_string() const;
+
+  // Create an Address from a 32-bit raw numeric IP address
+  static Address from_ipv4_numeric(uint32_t ip_address);
   //!@}
 
   //! \name Low-level operations
   //!@{
-
-  //! Size of the underlying address storage.
+  // (Getter) Size of the underlying address storage.
   socklen_t size() const { return size_; }
-  //! Const pointer to the underlying socket address storage.
-  const sockaddr* raw() const { return static_cast<const sockaddr*>( address_ ); }
-  //! Safely convert to underlying sockaddr type
-  template<typename sockaddr_type>
-  const sockaddr_type* as() const;
-
+  // Const pointer to the underlying socket address storage.
+  const sockaddr *raw() const { return static_cast<const sockaddr *>(address_); }
+  // Safely convert to underlying sockaddr type
+  template <typename sockaddr_type>
+  const sockaddr_type *as() const;
   //!@}
 };
